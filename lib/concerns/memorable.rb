1fb5b291c002cc4093288a6a9d3cee59129ae04f
Hossam Hosny <hossam.hosni.95@gmail.com>
2017-09-05 17:13:50 +0000

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,16 @@
-module memorable
-
+module Memorable
+	module ClassMethods
 	  def reset_all
 	    self.all.clear
 	  end
 
 	  def count
-	    @@artists.count
+	    self.all.count
 	  end
+	end
+	module InstanceMethods
+		def initialize
+			self.class.all << self
+		end
+	end
 end
